7b148b4aff2eb5bc6bcda6c5384b10da32309cfc
Matthew Zakari <zakarimatthew62@gmail.com>
2023-08-22 14:15:02 -0700

    C-printf

---
+++ 0_C-printf/_printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int char_count = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			/* Print regular character */
+			write(1, format, 1);
+			char_count++;
+		}
+		else
+		{
+			format++; /* Move to the next character after '%' */
+
+			/* Handle format specifiers */
+			switch (*format)
+			{
+				case 'c':
+					print_char(args, &char_count);
+					break;
+				case 's':
+					print_string(args, &char_count);
+					break;
+				case '%':
+					print_percent(&char_count);
+					break;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (char_count);
+}
+
